82722db1d3d72337cf4d3f0209ed66b3678f2124
Clint Liddick <clint.liddick@gmail.com>
2017-03-27 11:11:04 -0400

    don't set vim as git mergetool

--- gitconfig.sh
+++ gitconfig.sh
@@ -4,6 +4,6 @@ git config --global user.email "clint.liddick@gmail.com"
 # add `git lol` pretty log command
 git config --global --add alias.lol "log --graph --decorate --pretty=oneline --abbrev-commit --all"
 # set vim fugitive as merge tool
-git config --global mergetool.fugitive.cmd 'vim -f -c "Gvdiff" "$MERGED"'
-git config --global merge.tool fugitive
-git config --global push.default simple # until git 2 this becomes default
+# git config --global mergetool.fugitive.cmd 'vim -f -c "Gvdiff" "$MERGED"'
+# git config --global merge.tool fugitive
+# git config --global push.default simple # until git 2 this becomes default
