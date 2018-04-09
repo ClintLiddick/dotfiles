157bac374482cc9de4982878c3c9db284c0f3ef0
Clint Liddick <clint@aurora.tech>
2018-04-09 14:56:00 -0400

    git lol update

--- gitconfig.sh
+++ gitconfig.sh
@@ -2,7 +2,7 @@
 git config --global user.name "Clint Liddick"
 git config --global user.email "clint.liddick@gmail.com"
 # add `git lol` pretty log command
-git config --global --add alias.lol "log --graph --decorate --pretty=oneline --abbrev-commit --all"
+git config --global --add alias.lol "log --graph --abbrev-commit --all --decorate --pretty=format:'%C(auto)%h %d %s - [%aN]'"
 # set vim fugitive as merge tool
 # git config --global mergetool.fugitive.cmd 'vim -f -c "Gvdiff" "$MERGED"'
 # git config --global merge.tool fugitive
