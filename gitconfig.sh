1ac6fc5cb4563f2955165639e8392bdb0334ebda
Clint Liddick <clint.liddick@gmail.com>
2019-10-24 23:01:10 -0700

    [ansible] Use ansible playbooks for system configuration
    Cleanup unused files

--- gitconfig.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-git config --global user.name "Clint Liddick"
-git config --global user.email "clint.liddick@gmail.com"
-# add `git lol` pretty log command
-git config --global --add alias.lol "log --graph --abbrev-commit --all --decorate --pretty=format:'%C(auto)%h %d %s - [%aN]'"
-# set vim fugitive as merge tool
-# git config --global mergetool.fugitive.cmd 'vim -f -c "Gvdiff" "$MERGED"'
-# git config --global merge.tool fugitive
-# git config --global push.default simple # until git 2 this becomes default
