2c30d57517be0e67fa8029632447a0cc231ad854
Clint Liddick <clint@aurora.tech>
2020-09-24 12:37:52 -0700

    [emacs] Add bazel WORKSPACE file to Python mode

--- emacs.el
+++ emacs.el
@@ -256,6 +256,7 @@
   :mode "\\.proto\\'")
 
 ;; Bazel files are basically Python
+(add-to-list 'auto-mode-alist '("\\WORKSPACE\\'" . python-mode))
 (add-to-list 'auto-mode-alist '("\\BUILD\\'" . python-mode))
 (add-to-list 'auto-mode-alist '("\\.bzl\\'" . python-mode))
 
