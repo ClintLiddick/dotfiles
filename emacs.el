dd3061021a311474c48ba410fa55a5bb065f46cf
Clint Liddick <clint@aurora.tech>
2026-03-23 11:50:04 -0700

    [emacs] Add automatically added custom var and font statements

--- emacs.el
+++ emacs.el
@@ -553,3 +553,15 @@
 (global-prettify-symbols-mode t)
 
 ;;; emacs.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-vc-selected-packages '((majutsu :url "https://github.com/0WD0/majutsu"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
